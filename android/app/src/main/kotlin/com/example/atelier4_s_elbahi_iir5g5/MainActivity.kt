0629b0c60a410afd5d19671d8f9b0992853f9752
Saad-Elbahi <elbahi.saademsi.1@gmail.com>
2023-11-20 00:51:37 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/atelier4_s_elbahi_iir5g5/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.atelier4_s_elbahi_iir5g5
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/login_ecran.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class LoginEcran extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return SignInScreen();
+          } else {
+            User? user = snapshot.data;
+            return Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Email: ${user?.email}'),
+                  ElevatedButton(
+                    onPressed: () {
+                      FirebaseAuth.instance.signOut();
+                    },
+                    child: Text('Se déconnecter'),
+                  ),
+                ],
+              ),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+class SignInScreen extends StatelessWidget {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  void login(BuildContext context) async {
+    try {
+      UserCredential userCredential =
+          await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailController.text,
+        password: passwordController.text,
+      );
+      // Connexion réussie, faire quelque chose si nécessaire
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('Utilisateur non trouvé');
+      } else if (e.code == 'wrong-password') {
+        print('Mot de passe incorrect');
+      }
+    } catch (e) {
+      print('Erreur de connexion : $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            controller: emailController,
+            decoration: InputDecoration(
+              labelText: 'Email',
+            ),
+          ),
+          TextField(
+            controller: passwordController,
+            decoration: InputDecoration(
+              labelText: 'Mot de passe',
+            ),
+            obscureText: true,
+          ),
+          SizedBox(height: 16.0),
+          ElevatedButton(
+            onPressed: () {
+              login(context);
+            },
+            child: Text('Se connecter'),
+          ),
+        ],
+      ),
+    );
+  }
+}
